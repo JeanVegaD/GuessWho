79753a15597299ac65ef31cef187629a6ddaee61
Jean <jean0798@gmail.com>
2020-08-07 21:20:14 -0600

    content: add game window

--- code/front-end.py
+++ code/front-end.py
@@ -1,10 +1,15 @@
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk
+from tkinter import messagebox
 from tkinter import ttk
+import tkinter as tk
+
+from PIL import Image, ImageTk
+
+import os.path
 
 
-def main_window():
+def mainWindow():
     global main_window
     main_window=Tk()
     main_window.geometry("1010x750")
@@ -20,30 +25,111 @@ def main_window():
     nicknname=StringVar()
     entry_nickname=Entry(main_window,textvariable=nicknname,width=40,relief=FLAT, bg="#F2F2F2",fg="#0ECA9B",font=('Corbel',12)).place(x=350,y=331)
 
-    filedialog.askopenfilename(filetypes = (("Template files", "*.tplate")
-                                                        ,("HTML files", "*.html;*.htm")
-                                                        ,("All files", "*.*") ))
+    global init_game
+    init_game=False
+    #load file bottom
+    global btn_load_filetxt
+    btn_load_filetxt=tk.StringVar()
+    btn_load_filetxt.set("Seleccione un archivo")
+    btn_load_file=Button(main_window,textvariable=btn_load_filetxt,width=35,bg="#F2F2F2",fg="#0ECA9B",font=('Corbel',12),relief=FLAT,highlightthickness=0, bd=0,command=loadFile,anchor="w").place(x=350,y=399)
 
     #play bottom
     img_btn_play=Image.open("../src/images/play-button.png")
     img_resize_btn_play = img_btn_play.resize((70, 70), Image.ANTIALIAS)
     img_btn_play_aux = ImageTk.PhotoImage(img_resize_btn_play)
-    btn_play=Button(main_window,bg="white",image=img_btn_play_aux,relief=FLAT,highlightthickness=0, bd=0).place(x=460,y=460)
+    btn_play=Button(main_window,bg="white",image=img_btn_play_aux,relief=FLAT,highlightthickness=0, bd=0,command=initGame).place(x=460,y=460)
 
     #info bottom
     img_btn_info=Image.open("../src/images/info.png")
     img_resize_btn_info = img_btn_info.resize((30, 30), Image.ANTIALIAS)
     img_btn_btn_info_aux = ImageTk.PhotoImage(img_resize_btn_info)
-    btn_info=Button(main_window,bg="white",image=img_btn_btn_info_aux,relief=FLAT,highlightthickness=0, bd=0).place(x=730,y=500)    
+    btn_info=Button(main_window,bg="white",image=img_btn_btn_info_aux,relief=FLAT,highlightthickness=0, bd=0,command=infoWindow).place(x=730,y=500)    
 
     #help bottom
     img_btn_help=Image.open("../src/images/question.png")
     img_resize_img_btn_help = img_btn_help.resize((30, 30), Image.ANTIALIAS)
     img_btn_btn_help_aux = ImageTk.PhotoImage(img_resize_img_btn_help )
-    btn_info=Button(main_window,bg="white",image=img_btn_btn_help_aux,relief=FLAT,highlightthickness=0, bd=0).place(x=690,y=500)    
-
+    btn_info=Button(main_window,bg="white",image=img_btn_btn_help_aux,relief=FLAT,highlightthickness=0, bd=0,command=helpWindow).place(x=690,y=500)    
 
     main_window.mainloop()
 
 
-main_window()
+#Open the file explorer. Load and validate the selected file
+def loadFile():
+        init_game=False
+        filepath= filedialog.askopenfilename(filetypes = (("JSON files", "*.json"),("All files", "*.*") ))
+        
+        if(filepath!=""):
+            nombre_archivo, extension = os.path.splitext(filepath)
+            if(extension==".json"):
+                btn_load_filetxt.set(nombre_archivo)
+                init_game=True
+            else:
+                messagebox.showerror(title="File error", message="The file must be a .json")
+
+
+
+#Verify that everything is correct to start the game
+def initGame():
+    if(init_game):
+        if(nicknname!=""):
+            print("entre")
+        else:
+            messagebox.showerror(title="Nickname error", message="Nicknname cannot be empty")
+    else:
+        messagebox.showerror(title="File error", message="Select a valid file")
+
+
+#Infomation window
+def infoWindow():
+    global info_window
+    info_window=Toplevel()
+    info_window.geometry("345x500")
+    info_window.title("Info")
+    info_window.config(bg="#ffffff")
+    info_window.resizable(False, False)
+    info_window.grab_set()
+
+    #Fondo de la ventana
+    fondo=Image.open("../src/images/background_about.png")
+    fondo_aux = ImageTk.PhotoImage(fondo)
+    ins_fondo=Label(info_window,image=fondo_aux).place(x=-2,y=-2)
+
+    info_window.mainloop()
+
+#Help window
+def helpWindow():
+    global help_window
+    help_window=Toplevel()
+    help_window.geometry("345x500")
+    help_window.title("Help")
+    help_window.config(bg="#ffffff")
+    help_window.resizable(False, False)
+    help_window.grab_set()
+
+    #Fondo de la ventana
+    fondo=Image.open("../src/images/background_help.png")
+    fondo_aux = ImageTk.PhotoImage(fondo)
+    ins_fondo=Label(help_window,image=fondo_aux).place(x=-2,y=-2)
+
+    help_window.mainloop()
+
+def newGame():
+    global game_window
+    game_window=Tk()
+    game_window.geometry("1010x750")
+    game_window.title("Guess Who?")
+    game_window.config(bg="#ffffff")
+    game_window.resizable(False, False)
+    #Fondo de la ventana
+    fondo=Image.open("../src/images/background_game.png")
+    fondo_aux = ImageTk.PhotoImage(fondo)
+    ins_fondo=Label(game_window,image=fondo_aux).place(x=-2,y=-2)
+    
+
+
+    game_window.mainloop()
+
+
+#mainWindow()
+newGame()
